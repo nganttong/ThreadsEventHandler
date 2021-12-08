7080bc9c32de350de5af1dcf7a9442568423a80e
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-08 14:33:07 -0500

    lab completed

--- src/main/java/EventTracker.java
+++ src/main/java/EventTracker.java
@@ -12,17 +12,32 @@ public class EventTracker implements Tracker {
     }
 
     synchronized public static EventTracker getInstance() {
-        return null;
+        return INSTANCE;
+    }
+
+    @Override
+    public Map<String, Integer> tracker() {
+        return tracker;
     }
 
     synchronized public void push(String message) {
+        if(has(message)) {
+            tracker.replace(message, tracker.get(message) + 1);
+        } else {
+            tracker.put(message, 1);
+        }
     }
 
     synchronized public Boolean has(String message) {
-        return null;
+        return this.tracker.containsKey(message)
+                && (this.tracker.get(message) > 0);
     }
 
     synchronized public void handle(String message, EventHandler e) {
+        e.handle();
+        if (has(message)) {
+            tracker.replace(message, tracker.get(message) - 1);
+        }
     }
 
     // Do not use this. This constructor is for tests only
